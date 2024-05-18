463ef76bd97439f2d80ea9bdb226cfe823eeea2c
hani momo <137775574+hani-momo@users.noreply.github.com>
2024-05-18 23:45:25 +0300

    Update Task_3.3.py

--- practical_course/Module_3/Task_3.3.py
+++ practical_course/Module_3/Task_3.3.py
@@ -1,6 +1,9 @@
 def create_num(m):
-    m.sort(reverse=True)  # массив наоборот
     result = ''.join(str(num) for num in m) # преобразование в строку
+    result = list(result)
+    '''m.sort(reverse=True)  # массив наоборот'''
+    result = sorted(result, reverse = True)
+    result = ''.join(result)
     return result
 
 massive = input('Массив через пробел: ')
@@ -8,4 +11,3 @@ massive = massive.split()
 massive = [int(num) for num in massive]
 print(massive)
 print(create_num(massive))
-
